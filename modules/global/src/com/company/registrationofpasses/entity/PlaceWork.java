3b429789146db81a8b7db92ff107b66003483210
Danil Ostroverh <ostroverh@list.ru>
2023-05-15 08:45:09 +0500

    create new entity PlaceWork

---
+++ modules/global/src/com/company/registrationofpasses/entity/PlaceWork.java
@@ -0,0 +1,12 @@
+package com.company.registrationofpasses.entity;
+
+import com.haulmont.cuba.core.entity.StandardEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Table(name = "REGISTRATIONOFPASSES_PLACE_WORK")
+@Entity(name = "registrationofpasses_PlaceWork")
+public class PlaceWork extends StandardEntity {
+    private static final long serialVersionUID = 3874625550550514090L;
+}
