3170f6aa95c78483ce316bf10543c7d5723f5bea
Danil Ostroverh <ostroverh@list.ru>
2023-05-16 20:39:58 +0500

    create new entity Phone

---
+++ modules/global/src/com/company/registrationofpasses/entity/Phone.java
@@ -0,0 +1,12 @@
+package com.company.registrationofpasses.entity;
+
+import com.haulmont.cuba.core.entity.StandardEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Table(name = "REGISTRATIONOFPASSES_PHONE")
+@Entity(name = "registrationofpasses_Phone")
+public class Phone extends StandardEntity {
+    private static final long serialVersionUID = -7954540123184299762L;
+}
