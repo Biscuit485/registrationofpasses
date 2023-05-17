e758de536817e0dec8f5e7fcd535cc02d34d6790
Danil Ostroverh <ostroverh@list.ru>
2023-05-17 10:17:19 +0500

    create entity PlaceResidence

---
+++ modules/global/src/com/company/registrationofpasses/entity/PlaceResidence.java
@@ -0,0 +1,12 @@
+package com.company.registrationofpasses.entity;
+
+import com.haulmont.cuba.core.entity.StandardEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Table(name = "REGISTRATIONOFPASSES_PLACE_RESIDENCE")
+@Entity(name = "registrationofpasses_PlaceResidence")
+public class PlaceResidence extends StandardEntity {
+    private static final long serialVersionUID = -3925017639164982363L;
+}
